980a821d699e18d9d0980681c12eb02199c14b54
Pushkar Borkar <pushkarborkar1809@gmail.com>
2022-07-10 05:41:33 +0530

    feat: Make Reusable Badge Component

---
+++ components/common/Badge.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import Styles from "../../styles/Common.module.scss";
+
+const Badge: FC = () => {
+	return <div className={Styles.badge}>Badge</div>;
+};
+
+export default Badge;
